0d882ad510c640315e4b6b4c06df820958a0af4e
JF Hector <jfhector@kooth.com>
2024-04-03 12:06:55 +0100

    copying from array rather than creating a memory view

---
+++ raschka-intro/10_copying_from_array_rather_than_creating_a_memory_view.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([[1,2,3], 
+                [4,5,6]])
+
+# Basic indexing and slicing routines create memory views of (references to positions in) the original ndarray, not copies
+# fst_row = arr[0]
+# fst_row += 10
+# arr
+
+fst_row_copy = arr[0].copy()
+fst_row += 10
+arr
