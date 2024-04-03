df0b2b56663e6d833f942e8a9359c8c1c53fceab
JF Hector <jfhector@kooth.com>
2024-04-03 10:40:31 +0100

    shape of an ndarray

---
+++ raschka-intro/02_shape-of-ndarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+lst = [[1,2,3],
+       [4,5,6]]
+
+# ary2d = np.array(lst, dtype=np.int32)
+ary2d = np.array(lst)
+ary2d = ary2d.astype(np.float32)
+
+# ary2d.dtype
+# ary2d.size
+# ary2d.ndim
+ary2d.shape
